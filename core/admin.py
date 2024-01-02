043abee6fe4b8abfaecad2e16e94d99f58ffd3e6
Ruhin Shukurlu <ruhinshukurlu@gmail.com>
2024-01-03 01:29:40 +0400

    admin.py updated

--- core/admin.py
+++ core/admin.py
@@ -3,11 +3,20 @@ from core.models import Company, Reservation, CompanyReview
 from django.db import models
 from django_json_widget.widgets import JSONEditorWidget
 
+
 @admin.register(Company)
-class YourModelAdmin(admin.ModelAdmin):
+class CompanyModelAdmin(admin.ModelAdmin):
+    list_display = ["name", "user","address", "created_at"]
     formfield_overrides = {
         models.JSONField: {'widget': JSONEditorWidget},
     }
 
-admin.site.register(Reservation)
-admin.site.register(CompanyReview)
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ["company", "user", "date", "time", "car_type"]
+
+
+@admin.register(CompanyReview)
+class CompanyReviewAdmin(admin.ModelAdmin):
+    list_display = ["company", "user", "rating", "created_at"]
